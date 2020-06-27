3e0c59316bcbacdbb6bd5e29007182a223020add
Work H <aaronblutstein@Work-H.local>
2020-06-27 23:06:09 +0100

    Updates

---
+++ precache-manifest.c14bb16f26d88377d9aecefabf925b5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af67e28f0d7f9fc7fe5a1f6599449f5",
+    "url": "/ilana-list/index.html"
+  },
+  {
+    "revision": "9fd5e9e319c18b48bb50",
+    "url": "/ilana-list/static/css/main.77dc9891.chunk.css"
+  },
+  {
+    "revision": "9caffe3a37b218e82327",
+    "url": "/ilana-list/static/js/2.39631657.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ilana-list/static/js/2.39631657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd5e9e319c18b48bb50",
+    "url": "/ilana-list/static/js/main.80a1903a.chunk.js"
+  },
+  {
+    "revision": "e59f813a28ef76f1374c",
+    "url": "/ilana-list/static/js/runtime-main.6f75f18e.js"
+  },
+  {
+    "revision": "1141a33b0961134617b6008221adba80",
+    "url": "/ilana-list/static/media/ButterflyKidsRegular.1141a33b.ttf"
+  },
+  {
+    "revision": "22ce3dc6a2c3f87b39f5bc8789257c44",
+    "url": "/ilana-list/static/media/LondrinaSketchRegular.22ce3dc6.ttf"
+  },
+  {
+    "revision": "43ee340b7f94cb2df2723af997f02aa3",
+    "url": "/ilana-list/static/media/RainbowPic.43ee340b.jpg"
+  }
+]);
